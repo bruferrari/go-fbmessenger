db96feddfa6207c106f9ef5dde46146f96ad8874
Bruno Ferrari <b.ferrari09@gmail.com>
2017-01-30 02:04:49 -0200

    implementing...

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import macaron "gopkg.in/macaron.v1"
-import "github.com/bruferrari/go-fbmessenger/fblib"
+import (
+	"log"
+
+	"github.com/bruferrari/go-fbmessenger/fblib"
+	macaron "gopkg.in/macaron.v1"
+)
 
 func macaronSetUp() {
 	m := macaron.Classic()
 
-	m.Get("/", func() string {
-		return "1951683431"
+	m.Get("/", func(ctx *macaron.Context) {
+		url := ctx.Req.URL
+		params := url.Query()
+
+		log.Println(url)
+		log.Println(params["hub.mode"])
+
+		if params["hub.mode"][0] == "subscribe" {
+			log.Println("HELL YEAH!")
+		}
+		log.Println(":(")
 	})
 
 	m.Run()
