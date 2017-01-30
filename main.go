73ccfc85c73611825be9297a9ad76c9f29586f95
Bruno Ferrari <b.ferrari09@gmail.com>
2017-01-30 02:42:18 -0200

    refactoring

--- main.go
+++ main.go
@@ -12,24 +12,33 @@ import (
 func macaronSetUp() {
 	m := macaron.Classic()
 
-	m.Get("/", func(ctx *macaron.Context) (int, string) {
-		url := ctx.Req.URL
-		params := url.Query()
-
-		log.Println(url)
-		log.Println(params["hub.mode"])
-
-		if params["hub.mode"][0] == "subscribe" {
-			if params["hub.verify_token"][0] != os.Getenv("VERIFY_TOKEN") {
-				log.Println("403", "Verification token mismatch")
-				return 403, "Verification token mismatch"
-			}
-			return http.StatusOK, params["hub.challenge"][0]
-		}
-		return http.StatusBadRequest, "Bad request"
-	})
+	m.Get("/", rootHandler)
 
 	m.Run()
+}
+
+func rootHandler(ctx *macaron.Context) (int, string) {
+	var resp string
+	if ok, resp := webhook(ctx); ok != false {
+		return http.StatusOK, resp
+	}
+	return http.StatusUnauthorized, resp
+}
+
+func webhook(ctx *macaron.Context) (bool, string) {
+	url := ctx.Req.URL
+	params := url.Query()
+
+	log.Println(url)
+	log.Println(params["hub.mode"])
+
+	if params["hub.mode"][0] == "subscribe" {
+		if params["hub.verify_token"][0] == os.Getenv("VERIFY_TOKEN") {
+			log.Println("Verification token mismatch")
+			return true, params["hub.challenge"][0]
+		}
+	}
+	return false, ""
 }
 
 func main() {
